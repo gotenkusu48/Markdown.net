64f97a6af696c5d6b14a822cbfc80c5c0c1b1e5d
久米　史也 <guragura@outlook.com>
2015-12-02 16:34:13 +0900

    Init

---
+++ MarkDown.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MarkDown;
+using System.Text;
+
+namespace MarkDown
+{
+    public class MarkDown
+    {
+
+        public string Markdowntext { get; }
+
+        public MarkDown(string text)
+        {
+            this.Markdowntext = text;
+
+            var taginfos = new List<TagGenerater.TagInfo>();
+            
+
+            var lineBreakCount = 0;
+        }
+
+    }
+}
---
+++ TagGenerater.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkDown
+{
+    public class TagGenerater
+    {
+        public enum TagType
+        {
+            H1, H2, H3, H4, H5,
+            P, Strong
+        }
+
+        public class TagInfo
+        {
+            public TagType TagType { get; set; }
+            public string text { get; set; } = "";
+            public string link { get; set; } = "";
+        }
+
+        public static string TagGenerate(TagInfo info)
+        {
+            return "";
+        }
+
+        /// <summary>
+        /// this is a generate for h TAG
+        /// </summary>
+        /// <param name="text">inner Text</param>
+        /// <param name="count">h type</param>
+        /// <returns></returns>
+        public string Hn(string text, int count)
+        {
+            return $"<h{count}>{text}</h{count}>";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public string SingleText(string text)
+        {
+            return $"<p>{text}</p>";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public string strong(string text)
+        {
+            return $"<strong>{text}</strong>";
+        }
+
+
+
+    }
+}
